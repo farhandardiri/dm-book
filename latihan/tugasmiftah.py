eaced5ea5509d2cf6747ab1a788a9f96c4d1b00c
farhandardiri <faran.f4124n@gmail.com>
2021-04-01 14:49:16 +0700

    add file tugas miftah

---
+++ latihan/tugasmiftah.py
@@ -0,0 +1,24 @@
+nama = "2"
+umur = 23
+mahasiswa = True
+print("Nama saya adalah", nama, "Type =", type(nama))
+print("Umur saya adalah", umur, "Type =", type(umur))
+print("Apakah saya mahsiswa ? ", mahasiswa, "Type =", type(mahasiswa))
+
+print(int(nama))
+print(str(nama))
+print(float(nama))
+print(bool(nama))
+
+nama = input("masukkan nama anda :")
+
+umur = int(input("masukkan umur anda :"))
+nilai = float(input("masukkan nilai anda :"))
+
+nilai1 = input("massukkan nilai pertama")
+formatnilai = float(nilai1)
+
+print(nama)
+print(umur)
+print(nilai)
+print(formatnilai)
